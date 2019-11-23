3488b282bbc42c8c380f948b2d1d396d73b9c813
root <root@ip-10-0-0-36.ap-northeast-1.compute.internal>
2019-11-23 10:44:32 +0000

    20191123_01

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,14 +1,28 @@
-APP_PATH = "/var/www/ruby/rss"
-
-worker_processes 2
-
-working_directory "/var/www/ruby/rss"
-
-listen 3000
-
-pid APP_PATH + "/tmp/pids/unicorn.pid"
-stderr_path APP_PATH + "/log/unicorn.log"
-stdout_path APP_PATH + "/log/unicorn.log"
-
-
-preload_app true
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+ timeout 15
+ preload_app true
+ 
+ listen '/var/www/Ciel-SLIP/tmp/unicorn.sock'
+ pid    '/var/www/Ciel-SLIP/tmp/unicorn.pid'
+ 
+ before_fork do |server, worker|
+   Signal.trap 'TERM' do
+     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+     Process.kill 'QUIT', Process.pid
+   end
+ 
+   defined?(ActiveRecord::Base) and
+     ActiveRecord::Base.connection.disconnect!
+ end
+ 
+ after_fork do |server, worker|
+   Signal.trap 'TERM' do
+     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+   end
+ 
+   defined?(ActiveRecord::Base) and
+     ActiveRecord::Base.establish_connection
+ end
+ 
+ stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+ stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -4,8 +4,10 @@ working_directory @app_path + "/current"
 worker_processes 2
 preload_app true
 timeout 30
-listen "/tmp/unicorn.sock", :backlog => 64
-pid "/var/www/{appbane}/shared/tmp/pids/unicorn.pid"
+#listen "/tmp/unicorn.sock", :backlog => 64
+#pid "/var/www/{appbane}/shared/tmp/pids/unicorn.pid"
+listen "/var/run/unicorn.sock"
+pid "/var/run/unicorn.pid"
 
 stderr_path "#{@app_path}/log/unicorn.stderr.log"
 stdout_path "#{@app_path}/log/unicorn.stdout.log"
